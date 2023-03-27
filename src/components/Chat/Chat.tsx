f8cef0809433f8b7a4a629e2c4746fafa9ad9897
Pavol Hejny <me@pavolhejny.com>
2023-03-28 00:15:09 +0200

    Design of chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 import { Promisable } from 'type-fest';
+import { classNames } from '../../utils/classNames';
 import { Article } from '../Article/Article';
 import styles from './Chat.module.css';
 export interface ChatMessage {
@@ -32,27 +33,38 @@ export function Chat(props: ChatProps) {
 
     return (
         <div className={styles.Chat}>
-            {messages.map(({ content }, i) => (
-                <Article key={i} {...{ content }} />
-            ))}
+            <div className={styles.chatMessages}>
+                {messages.map(({ from, content }, i) => (
+                    <div key={i} className={classNames(styles.chatMessage, from === 'TEACHER' && styles.sender)}>
+                        <div key={i} className={styles.sender}>
+                            {from.substring(0, 1)}
+                        </div>
+                        <div key={i} className={styles.messageText}>
+                            <Article /* TODO: className={styles.messageText} */ {...{ content }} />
+                        </div>
+                    </div>
+                ))}
+            </div>
 
-            <textarea
-                ref={textAreaElement as any} /* <- !!! Focus on page load */
-                defaultValue=""
-                placeholder="Napište zprávu"
-                onKeyDown={(event) => {
-                    if (event.shiftKey) {
-                        return;
-                    }
-                    if (event.key !== 'Enter') {
-                        return;
-                    }
+            <div className={styles.chatInput}>
+                <textarea
+                    ref={textAreaElement as any} /* <- !!! Focus on page load */
+                    defaultValue=""
+                    placeholder="Napište zprávu"
+                    onKeyDown={(event) => {
+                        if (event.shiftKey) {
+                            return;
+                        }
+                        if (event.key !== 'Enter') {
+                            return;
+                        }
 
-                    event.preventDefault();
-                    /* not await */ handleSend();
-                }}
-            />
-            <button onClick={/* not await */ handleSend}>Odeslat</button>
+                        event.preventDefault();
+                        /* not await */ handleSend();
+                    }}
+                />
+                <button onClick={/* not await */ handleSend}>Odeslat</button>
+            </div>
         </div>
     );
 }
