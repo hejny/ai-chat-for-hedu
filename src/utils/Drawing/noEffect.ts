a2f3c5ad17e50ee93697461dc94f5bfbdb6f2017
Pavol Hejny <me@pavolhejny.com>
2023-03-28 00:15:38 +0200

    Remove boilerplate code

--- src/utils/Drawing/noEffect.ts
+++
@@ -1,8 +0,0 @@
-import { IDestroyable, Registration } from 'destroyable';
-
-/**
- * 
- */
-export function noEffect<TElement extends HTMLElement>(element: TElement): IDestroyable {
-    return Registration.void();
-}
