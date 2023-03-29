ad02fd7d6030c66c96899dafaff7192efb0d224a
Pavol Hejny <me@pavolhejny.com>
2023-03-29 16:21:19 +0200

    Add important TODOs for VoiceRecognitionButton

--- src/components/VoiceRecognitionButton/VoiceRecognitionButton.tsx
+++ src/components/VoiceRecognitionButton/VoiceRecognitionButton.tsx
@@ -40,6 +40,8 @@ export function VoiceRecognitionButton(props: VoiceRecognitionButtonProps) {
                             .map((result: any) => result[0].transcript)
                             .join('');
 
+                        // TODO: !!! Append the text do NOT override
+                        // TODO: !!! Instert the text where the cursor is
                         textareaRef.current!.value = transcript;
                     });
 
@@ -55,6 +57,9 @@ export function VoiceRecognitionButton(props: VoiceRecognitionButtonProps) {
 }
 
 /**
+ * TODO: !!! Better design of the VoiceRecognitionButton
+ * TODO: !!! Stop recording when stop speaking
+ * TODO: !!! Stop recording when unfocus textarea
  * TODO: !!! Reset recording after send
  * TODO: !!! Conversation mode - auto switch between dictation and speech
  * TODO: !!! Auto sent - with "odeslat" OR longer pause
