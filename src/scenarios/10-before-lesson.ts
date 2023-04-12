bc1a68820da92714ec6952a44a65e0122eb72abc
Pavol Hejny <me@pavolhejny.com>
2023-04-12 17:34:26 +0200

    Cleanup beforeLessonScenario
    (samples are organized in separate files)

--- src/scenarios/10-before-lesson.ts
+++ src/scenarios/10-before-lesson.ts
@@ -1,4 +1,4 @@
-import spaceTrim from 'spacetrim';
+import { forTime } from 'waitasecond';
 import { ScenarioUtils } from '../model/_';
 
 export async function beforeLessonScenario({
@@ -10,40 +10,12 @@ export async function beforeLessonScenario({
     load,
     summarize,
 }: ScenarioUtils): Promise<void> {
-    while (true) {
-        await say(`A`);
-        await say(`B`);
-        await say(`C`);
-        const response = await ask(`Napiš cokoliv`);
-        await say(
-            await spaceTrim(
-                async (block) => `
-                    Napsal jsi:
-
-                    \`\`\`
-                    ${block(await response.content.asPromise())}
-                    \`\`\`
-                `,
-            ),
-        );
-    }
-
-    /*
-    const response = await askOptions(`V jaké fázi jsi?`, { beforeLesson: `Před hodinou`, afterLesson: `Po hodině` });
-    await say(`Zvolena možnost "${response}"`);
-
-    /*
-    // TODO: !!! Make following scenario active:
-
-    console.log('beforeLessonScenario', 1);
     const lessonPlan = await ask(rewrite(`Jaký máš cíl dnešní hodiny matematiky?`));
     await save({ lessonPlan });
 
-    console.log('beforeLessonScenario', 2);
     const primaryLearningMaterial = await ask(rewrite(`Jaké úlohy plánuješ dnes zadat žákům z učebnice?`));
     await save({ primaryLearningMaterial });
 
-    console.log('beforeLessonScenario', 3);
     const secondaryLearningMaterial = await ask(rewrite(`Co s nimi plánuješ dělat dál mimo učebnici?`));
     await save({ secondaryLearningMaterial });
 
@@ -52,6 +24,4 @@ export async function beforeLessonScenario({
 
     console.log('beforeLessonScenario', 4);
     await say(rewrite(`Zkus se dnes soustředit více na žáka X`));
-
-    */
 }
