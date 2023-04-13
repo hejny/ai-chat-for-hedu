19ca233b46a8df1ec4598be65dcad5cc493db393
Pavol Hejny <me@pavolhejny.com>
2023-04-13 12:46:00 +0200

    Sample waitScenario is working

--- src/socket/initializeChatSocket.ts
+++ src/socket/initializeChatSocket.ts
@@ -1,10 +1,10 @@
 import { Socket } from 'socket.io';
 import { RunningScenario } from '../model/RunningScenario';
-import { randomScenario } from '../scenarios/samples/random';
+import { lessonScenario } from '../scenarios/00-lesson';
 import { SocketEventMap } from './SocketEventMap';
 
 export async function initializeChatSocket(connection: Socket<SocketEventMap>): Promise<void> {
-    const runningScenario = new RunningScenario(randomScenario /* <- TODO: !!! Put here lessonScenario */, connection);
+    const runningScenario = new RunningScenario(lessonScenario, connection);
 
     connection.on('disconnect', () => {
         /* not await */ runningScenario.destroy();
