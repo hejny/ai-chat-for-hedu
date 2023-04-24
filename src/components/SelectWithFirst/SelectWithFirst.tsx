b749fe283ec0a2fa2ca0ca58c6aa29a18be9e479
Pavol Hejny <me@pavolhejny.com>
2023-04-24 19:41:51 +0200

    RecordsFilter

--- src/components/SelectWithFirst/SelectWithFirst.tsx
+++ src/components/SelectWithFirst/SelectWithFirst.tsx
@@ -5,37 +5,43 @@ interface SelectWithFirstProps<TValue> {
     title: string;
     value: TValue;
     onChange(newValue: TValue): void;
+    numberOfButtons?: number;
     options: Array<{ id: TValue; title: string }>;
 }
 
 export function SelectWithFirst<TValue extends string | number>(props: SelectWithFirstProps<TValue>) {
-    const { title, value, onChange, options } = props;
+    const { title, value, onChange, numberOfButtons = 1 } = props;
 
-    const [firstOption, ...restOptions] = options;
-
-    if (!firstOption) {
-        return <></>;
-    }
+    const options = [...props.options];
+    const firstOptions = options.slice(0, numberOfButtons);
+    const restOptions = options.slice(numberOfButtons);
 
     return (
         <div className={styles.SelectWithFirst}>
-            {title}{' '}
-            <button
-                onClick={() => void onChange(firstOption.id)}
-                className={classNames(value === firstOption.id && styles.selected)}
-            >
-                {firstOption.title}
-            </button>
-            <select
-                onChange={(event) => void onChange(parseInt(event.target.value) as any)}
-                className={classNames(value !== firstOption.id && styles.selected)}
-            >
-                {restOptions.map((option) => (
-                    <option key={option.id} selected={value === option.id} value={option.id}>
-                        {option.title}
-                    </option>
-                ))}
-            </select>
+            <span className={styles.title}>{title}</span>
+
+            {firstOptions.map((option) => (
+                <button
+                    key={option.id}
+                    onClick={() => void onChange(option.id)}
+                    className={classNames(value === option.id && styles.selected)}
+                >
+                    {option.title}
+                </button>
+            ))}
+
+            {restOptions.length !== 0 && (
+                <select
+                    onChange={(event) => void onChange(parseInt(event.target.value) as any)}
+                    className={classNames(!firstOptions.some(({ id }) => id === value) && styles.selected)}
+                >
+                    {restOptions.map((option) => (
+                        <option key={option.id} selected={value === option.id} value={option.id}>
+                            {option.title}
+                        </option>
+                    ))}
+                </select>
+            )}
         </div>
     );
 }
--- src/sections/Records/RecordsFilter/RecordsFilter.tsx
+++ src/sections/Records/RecordsFilter/RecordsFilter.tsx
@@ -7,6 +7,8 @@ export function RecordsFilter(props: RecordsFilterProps) {
     const {} = props;
 
     const [person, setPerson] = useState(0);
+    const [sumarizationStyle, setSumarizationStyle] = useState(0);
+    const [subject, setSubject] = useState(0);
 
     return (
         <div>
@@ -24,6 +26,28 @@ export function RecordsFilter(props: RecordsFilterProps) {
                     { id: 5, title: `Kateřina Kšírová` },
                     { id: 6, title: `Josef Urban` },
                     { id: 7, title: `Josef Červenka` },
+                ]}
+            />
+            <SelectWithFirst
+                title={``}
+                value={sumarizationStyle}
+                onChange={(newSumarizationStyle) => void setSumarizationStyle(newSumarizationStyle)}
+                numberOfButtons={2}
+                options={[
+                    { id: 0, title: `Sumarizace` },
+                    { id: 1, title: `Podrobně` },
+                ]}
+            />
+            <SelectWithFirst
+                title={`Předmět`}
+                value={subject}
+                onChange={(newSubject) => void setSubject(newSubject)}
+                options={[
+                    { id: 0, title: `Vše` },
+                    { id: 1, title: `Matematika` },
+                    { id: 2, title: `Prvouka` },
+                    { id: 3, title: `Český jazyk` },
+                    { id: 4, title: `Anglický jazyk` },
                 ]}
             />
         </div>
